640e047865f86620e3af6ce05ec32f5cff440ea3
Aaron Brady <aaron@overlookresearch.com>
2015-07-26 12:33:42 -0700

    foo

--- dsscapstone/quiz2.R
+++ dsscapstone/quiz2.R
@@ -8,8 +8,25 @@ twoGram <- fread('twoGramFreqDT.RData')
 oneGram <- oneGram[order(-freq)]
 twoGram <- twoGram[order(-freq)]
 
-q0 <- tokenizeLines('The guy in front of me just bought a pound of bacon, a bouquet, and a case of', 'en')
-twoGram[tok0==q0[length(q0)]]
-predict <- function(sentence) {
-  twoGram[s]
+predictMatches <- function(sentence, potentialAnswers) {
+  toks <- tokenizeLines(sentence, 'en');
+  lastTok <- toks[[length(toks)]]
+  DT <- twoGram[tok0==lastTok & tok1 %in% potentialAnswers];    
+  DT[order(-freq)]
 }
+
+# q1
+s <- 'The guy in front of me just bought a pound of bacon, a bouquet, and a case'
+ans <- c('beer','cheese','soda','pretzels')
+predictMatches(s,ans)
+
+# q2
+s <- "You're the reason why I smile everyday. Can you follow me please? It would mean the"
+ans <- c('best','most','world','universe');
+predictMatches(s,ans)
+
+# q3
+s <- "Hey sunshine, can you follow me and make me the"
+ans <- c('happiest','saddest','bluest','smelliest')
+predictMatches(s,ans)
+# no matches???
